2109582a5a656bf3ae439774297e4469371237cd
jan <janek.zapal@gmail.com>
2018-03-20 22:34:51 +0100

    deployment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 export DJANGO_SETTINGS_MODULE=production_settings
+
+find . -name '*.pyc' -print0 | xargs -0 rm
 ./manage.py collectstatic
 ./manage.py compress
 ./manage.py migrate --database=default
