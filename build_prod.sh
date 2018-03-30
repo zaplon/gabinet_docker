8eecc1500b403205f85617749ec82e794767432c
jan <janek.zapal@gmail.com>
2018-03-30 13:23:09 +0200

    changes

--- build_prod.sh
+++ build_prod.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-docker build docker/web -t docker.io/zaplon/gabinet_prod:latest
-docker push docker.io/zaplon/gabinet_prod:latest
+docker-compose run web gabinet_docker/web/build.sh
+docker build . -f gabinet_docker/web/Dockerfile -t docker.io/zaplon/gabinet_prod:latest
+#docker push docker.io/zaplon/gabinet_prod:latest
--- web/build.sh
+++ web/build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 export DJANGO_SETTINGS_MODULE=production_settings
-find . -name '*.pyc' -print0 | xargs -0 rm
-echo yes | ./manage.py collectstatic
 echo yes | ./manage.py compress
+find . -name '*.pyc' -print0 | xargs -0 rm
